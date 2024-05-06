a86f344bc22594906ce04992e37f3f912fecbbc2
SnehaKallem <snehak@brightcomgroup.com>
2024-05-06 15:48:39 +0530

    added portfolio

--- src/components/Header1.js
+++ src/components/Header1.js
@@ -7,25 +7,21 @@ const Header1 = () => {
         <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
             <div className="container">
                 <a className="navbar-brand" href="/">
-                <img src="Sneha1.jpeg" alt="Profilepic" className="img-fluid rounded-circle" style={{ width: '30px', height: '30px' }} />
-
-
-                    Sneha Portfolio
+                    <img src="Sneha1.jpeg" alt="Profilepic" className="img-fluid rounded-circle" style={{ width: '30px', height: '30px' }} />
+                    Sneha Kallem
                 </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link" href="#about">About</a>
+                            <a className="nav-link" href="#home">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#skills">Skills</a>
+                            <a className="nav-link" href="#skills">About</a>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#experience">Experience</a>
-                        </li>
+                        
                         <li className="nav-item">
                             <a className="nav-link" href="#projects">Projects</a>
                         </li>
