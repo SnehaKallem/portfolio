5af748809ec2786ddc9ce79dff90991bd9809d05
SnehaKallem <snehak@brightcomgroup.com>
2024-05-06 15:32:30 +0530

    added portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,13 +3,14 @@ import About from './About'
 import Contact from './Contact'
 import Experience from './Experience'
 import Header from './Header'
+import Header1 from './Header1'
 import Projects from './Projects'
 import Skills from './Skills'
 
 function Portfolio() {
   return (
     <div>
-      <Header/>
+      <Header1/>
       <About/>
       <Skills/>
       <Experience/>
