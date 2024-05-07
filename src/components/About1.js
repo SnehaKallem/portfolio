da1b8608c6bf1dce86f91ee1a16f4ab391ed8378
SnehaKallem <snehak@brightcomgroup.com>
2024-05-07 16:25:23 +0530

    added portfolio

--- src/components/About1.js
+++ src/components/About1.js
@@ -5,29 +5,29 @@ import { Button } from '@mui/material';
 
 function About1() {
   return (
-    <section className="text-center " style={{marginTop:'150px'}} >
-      
-      <h1 style={{letterSpacing:'3px',fontWeight:800,fontSize:'2rem'}}>HEY, I'M SNEHA KALLEM</h1>
-      <div style={{ margin: '3rem auto 0 auto',maxWidth: '80rem' }}>
+    <><div id='about1'><section className="text-center " style={{ marginTop: '150px' }}>
+
+      <h1 style={{ letterSpacing: '3px', fontWeight: 800, fontSize: '2rem' }}>HEY, I'M SNEHA KALLEM</h1>
+      <div style={{ margin: '3rem auto 0 auto', maxWidth: '80rem' }}>
         <p>
-          A Full Stack Developer with a passion for building innovative web applications.<br/>
+          A Full Stack Developer with a passion for building innovative web applications.<br />
           Skilled in frontend and backend technologies, I aim to leverage my expertise to deliver
           high-quality solutions that meet client needs and drive business growth.
         </p>
 
       </div>
-      <div style={{    marginTop: '5rem',}}>
-      <Button variant="contained" color="secondary" href="/projects" style={{ marginTop: '1rem',width:'150px' }}>
-        Projects
-      </Button>
+      <div style={{ marginTop: '5rem', }}>
+        <Button variant="contained" color="secondary" href="#projects1" style={{ marginTop: '1rem', width: '150px' }}>
+          Projects
+        </Button>
       </div>
-      
+
 
       <div>
-        
+
       </div>
-     
-    </section>
+
+    </section></div></>
   );
 }
 
