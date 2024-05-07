67e1d31c8e250739199e5ebe27bc2c42d8c9b7f7
SnehaKallem <snehak@brightcomgroup.com>
2024-05-07 12:50:28 +0530

    added portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import About from './About'
 import About1 from './About1'
+import AboutMe from './AboutMe'
 import Contact from './Contact'
 import Experience from './Experience'
 import Header from './Header'
 import Header1 from './Header1'
 import Projects from './Projects'
+import Projects1 from './Projects1'
 import Skills from './Skills'
 
 function Portfolio() {
@@ -13,9 +15,11 @@ function Portfolio() {
     <div>
       <Header1/>
       <About1/>
-      <Skills/>
-      <Experience/>
-      <Projects/>
+      <AboutMe/>
+      <Projects1/>
+      {/* <Skills/> */}
+      {/* <Experience/> */}
+      {/* <Projects/> */}
       <Contact/>
     </div>
   )
